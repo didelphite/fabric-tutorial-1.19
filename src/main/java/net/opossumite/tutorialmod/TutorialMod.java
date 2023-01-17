11814df3352c4b8d4b61c99c8682b1c40c1e240e
opossumite <81266515+leftistsgorilla@users.noreply.github.com>
2023-01-16 23:43:16 -0800

    added item group for tanzanite

--- src/main/java/net/opossumite/tutorialmod/TutorialMod.java
+++ src/main/java/net/opossumite/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.opossumite.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.opossumite.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		ModItems.registermoditems();
+		ModItems.registerModItems();
 
 	}
 }
--- src/main/java/net/opossumite/tutorialmod/item/ModItems.java
+++ src/main/java/net/opossumite/tutorialmod/item/ModItems.java
@@ -1,19 +1,19 @@
-package net.opossumite.tutorialmod;
-
+package net.opossumite.tutorialmod.item;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.opossumite.tutorialmod.TutorialMod;
 
 public class ModItems {
 
     public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
     public static final Item TANZANITE = registerItem("tanzanite",
-            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+            new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
 
-    public static void registermoditems() {
+    public static void registerModItems() {
         TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
     }
 
