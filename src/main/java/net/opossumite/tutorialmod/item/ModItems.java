9bca25e083de4301d1087ab62708282cff56a58f
opossumite <81266515+leftistsgorilla@users.noreply.github.com>
2023-01-19 14:16:07 -0800

    hopefully fixed loot tables

--- src/main/java/net/opossumite/tutorialmod/item/ModItems.java
+++ src/main/java/net/opossumite/tutorialmod/item/ModItems.java
@@ -5,12 +5,15 @@ import net.opossumite.tutorialmod.TutorialMod;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.opossumite.tutorialmod.item.custom.EightBallItem;
 
 public class ModItems {
     public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
             new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
     public static final Item TANZANITE = registerItem("tanzanite",
             new Item(new FabricItemSettings().group(ModItemGroup.TANZANITE)));
+    public static final Item EIGHT_BALL = registerItem("eight_ball",
+                new EightBallItem(new FabricItemSettings().group(ModItemGroup.TANZANITE).maxCount(1)));
 
 
     private static Item registerItem(String name, Item item) {
@@ -20,4 +23,5 @@ public class ModItems {
     public static void registerModItems() {
         TutorialMod.LOGGER.debug("Registering Mod Items for " + TutorialMod.MOD_ID);
     }
+
 }
